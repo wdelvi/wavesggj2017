10eaecd69ee0249f96c0fa5091f97776e6d4d672
Willem Delventhal <will.delcenthal@gmail.com>
2017-01-21 00:39:54 -0800

    UI System now displaying “score” or at least a standin

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour 
 {
 	//All of our options for chunks
 	public List<GameObject> levelChunkOptions;
+
+	public Text meterCounter; 
 
 	private LevelController levelController;
 		
@@ -18,9 +21,17 @@ public class GameController : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
 	{
+		//Temporary function to move camera artifically
 		Vector3 newCameraPosition = Camera.main.transform.localPosition;
 		newCameraPosition.x += 0.1f;
 		Camera.main.transform.localPosition = newCameraPosition; 
+
+		this.UpdateUI ();
 		this.levelController.Update ();
+	}
+
+	void UpdateUI()
+	{
+		this.meterCounter.text = "" + ( Mathf.Round(Camera.main.transform.localPosition.x * 100 ) + Mathf.Round ( Random.Range( 0.0f, 0.99f ) * 100 ) / 100 ) + " M";  
 	}
 }
